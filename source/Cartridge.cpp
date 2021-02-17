cthornsb/OtterGB

commit 7e560621a2221dafe3e8c4b9e67e7d4b2b6c7ac1

Add support for Visual Studio
Source code now compiles using Visual Studio. Works with MSVC compiler building for x64. Other build types un-tested currently.

Added configuration files when using emulator on Windows (no command line arguments currently supported).

Emulator starts and runs on Windows 10 x64 but there seems to be issues with running certain roms hanging or crashing.

Qt debugger currently not supported on Windows.

source/Cartridge.cpp
@@ -28,103 +28,97 @@ bool Cartridge::preReadAction(){
 }
 
 bool Cartridge::writeRegister(const unsigned short &reg, const unsigned char &val){
-	switch(cartridgeType){
-		case 0x0: // ROM only
-			break;
-		case 0x1 ... 0x3: // MBC1
-			if(reg < 0x2000){ // RAM enable (write only)
-				// Any value written to this area with 0x0A in its lower 4 bits will enable cartridge RAM
-				// Note: Type 0x01 does not have cartridge RAM
-				extRamEnabled = ((val & 0x0F) == 0x0A);
-			}
-			else if(reg < 0x4000){ // ROM bank number register (write only)
-				// 5-bit register [0x01, 0x1F] (write only)
-				// Specify lower 5 bits of ROM bank number
-				bs &= 0xE0; // Clear bits 0-4
-				bs |= (val & 0x1F); // Set bits 0-4
-				// Note: MBC1 translates a 0x0 here as bank 1
-			}
-			else if(reg < 0x6000){
-				// 2-bit register [0,3] (write only)
-				if(cartridgeType == 0x1 || !ramSelect){ // Specify bits 5 & 6 of the ROM bank number (if in ROM select mode).
-					bs &= 0x9F; // Clear bits 5-6
-					bs |= ((val & 0x3) << 5); // Set bits 5-6
-				}
-				else // Specify RAM bank number (if in RAM select mode).
-					ram.setBank(val & 0x3);
-			}
-			else if(reg < 0x8000){ // ROM/RAM mode select
-				// 1-bit register which sets RAM/ROM mode select to 0x0 (ROM, default) or 0x1 (RAM)  
-				// Note: Type 0x01 does not have cartridge RAM
-				ramSelect = (val & 0x1) == 0x1;
-			}
-			break;
-		case 0x5 ... 0x6: // MBC2
-			if(reg < 0x2000){ // RAM enable (write only)
-				if((val & 0x100) == 0) // Bit 0 of upper address byte must be 0 to enable RAM
-					extRamEnabled = true;
-			}
-			else if(reg < 0x4000){ // ROM bank number (write only)
-				if((val & 0x100) != 0) // Bit 0 of upper address byte must be 1 to select ROM bank
-					bs = (val & 0x0F);
-			}
-			break;
-		case 0xB ... 0xD: // MMM01
-			break;
-		case 0xF ... 0x13: // MBC3
-			break;
-		case 0x15 ... 0x17: // MBC4
-			break;
-		case 0x19 ... 0x1E: // MBC5
-			if(reg < 0x2000){ // RAM enable (write only)
-				// Any value written to this area with 0x0A in its lower 4 bits will enable cartridge RAM
-				extRamEnabled = ((val & 0x0F) == 0x0A);
-			}
-			else if(reg < 0x3000){ // Lower 8 bits of ROM bank number register (write only)
-				// 9-bit register [0x01, 0x1FF] (write only)
-				// Specify lower 8 bits of ROM bank number
-				bs &= 0xFF00; // Clear bits 0-7
-				bs |= (val & 0x00FF); // Set bits 0-7
-			}
-			else if(reg < 0x4000){ // 9th bit of ROM bank number (write only)
-				// 1-bit register [0,3] (write only)
-				// Specify upper 1 bit of ROM bank number
-				bs &= 0xFEFF; // Clear bit 9;
-				bs |= ((val & 0x0001) << 8); // Set bit 9
-			}
-			else if(reg < 0x6000){ // RAM bank number
-				// Select the RAM bank [0,F]
-				ram.setBank(val & 0x0F);
-			}			
-			break;
-		default:
-			return false;
+	if(reg == 0x0){ // ROM only
 	}
-	return true;
+	else if(reg >= 0x1 && reg <= 0x3){ // MBC1
+		if(reg <= 0x2000){ // RAM enable (write only)
+			// Any value written to this area with 0x0A in its lower 4 bits will enable cartridge RAM
+			// Note: Type 0x01 does not have cartridge RAM
+			extRamEnabled = ((val & 0x0F) == 0x0A);
+		}
+		else if(reg < 0x4000){ // ROM bank number register (write only)
+			// 5-bit register [0x01, 0x1F] (write only)
+			// Specify lower 5 bits of ROM bank number
+			bs &= 0xE0; // Clear bits 0-4
+			bs |= (val & 0x1F); // Set bits 0-4
+			// Note: MBC1 translates a 0x0 here as bank 1
+		}
+		else if(reg < 0x6000){
+			// 2-bit register [0,3] (write only)
+			if(cartridgeType == 0x1 || !ramSelect){ // Specify bits 5 & 6 of the ROM bank number (if in ROM select mode).
+				bs &= 0x9F; // Clear bits 5-6
+				bs |= ((val & 0x3) << 5); // Set bits 5-6
+			}
+			else // Specify RAM bank number (if in RAM select mode).
+				ram.setBank(val & 0x3);
+		}
+		else if(reg < 0x8000){ // ROM/RAM mode select
+			// 1-bit register which sets RAM/ROM mode select to 0x0 (ROM, default) or 0x1 (RAM)  
+			// Note: Type 0x01 does not have cartridge RAM
+			ramSelect = (val & 0x1) == 0x1;
+		}
+	}
+	else if(reg >= 0x5 && reg <= 0x6){ // MBC2
+		if(reg < 0x2000){ // RAM enable (write only)
+			if((val & 0x100) == 0) // Bit 0 of upper address byte must be 0 to enable RAM
+				extRamEnabled = true;
+		}
+		else if(reg < 0x4000){ // ROM bank number (write only)
+			if((val & 0x100) != 0) // Bit 0 of upper address byte must be 1 to select ROM bank
+				bs = (val & 0x0F);
+		}
+	}
+	else if(reg >= 0xB && reg <= 0xD){ // MMM01
+	}
+	else if (reg >= 0xF && reg <= 0x13){ // MBC3
+	}
+	else if(reg >= 0x15 && reg <= 0x17){ // MBC4
+	}
+	else if(reg >= 0x19 && reg <= 0x1E){ // MBC5
+		if(reg < 0x2000){ // RAM enable (write only)
+			// Any value written to this area with 0x0A in its lower 4 bits will enable cartridge RAM
+			extRamEnabled = ((val & 0x0F) == 0x0A);
+		}
+		else if(reg < 0x3000){ // Lower 8 bits of ROM bank number register (write only)
+			// 9-bit register [0x01, 0x1FF] (write only)
+			// Specify lower 8 bits of ROM bank number
+			bs &= 0xFF00; // Clear bits 0-7
+			bs |= (val & 0x00FF); // Set bits 0-7
+		}
+		else if(reg < 0x4000){ // 9th bit of ROM bank number (write only)
+			// 1-bit register [0,3] (write only)
+			// Specify upper 1 bit of ROM bank number
+			bs &= 0xFEFF; // Clear bit 9;
+			bs |= ((val & 0x0001) << 8); // Set bit 9
+		}
+		else if(reg < 0x6000){ // RAM bank number
+			// Select the RAM bank [0,F]
+			ram.setBank(val & 0x0F);
+		}			
+	}
+	return false;
 }
 
 bool Cartridge::readRegister(const unsigned short &reg, unsigned char &val){
-	switch(cartridgeType){
-		case 0x0: // ROM only
-			break;
-		case 0x1 ... 0x3: // MBC1
-			// MBC1 has no registers which may be read
-			return false;
-		case 0x5 ... 0x6: // MBC2
-			// MBC2 has no registers which may be read
-			return false;
-		case 0xB ... 0xD: // MMM01
-			break;
-		case 0xF ... 0x13: // MBC3
-			break;
-		case 0x15 ... 0x17: // MBC4
-			break;
-		case 0x19 ... 0x1E: // MBC5
-			break;
-		default:
-			return false;
+	if(reg == 0x0){ // ROM only
 	}
-	return true;
+	else if(reg <= 0x1 && reg <= 0x3){ // MBC1
+		// MBC1 has no registers which may be read
+		return false;
+	}
+	else if(reg <= 0x5 && reg <= 0x6){ // MBC2
+		// MBC2 has no registers which may be read
+		return false;
+	}
+	else if(reg <= 0xB && reg <= 0xD){ // MMM01
+	}
+	else if(reg <= 0xF && reg <= 0x13){ // MBC3
+	}
+	else if(reg <= 0x15 && reg <= 0x17){ // MBC4
+	}
+	else if(reg <= 0x19 && reg <= 0x1E){ // MBC5
+	}
+	return false;
 }
 
 bool Cartridge::readRom(const std::string &fname, bool verbose/*=false*/){
@@ -132,7 +126,7 @@ bool Cartridge::readRom(const std::string &fname, bool verbose/*=false*/){
 	std::ifstream rom(fname.c_str(), std::ios::binary);
 	if(!rom.good())
 		return false;
-		
+
 	// Read the rom header
 	readHeader(rom);
 	
@@ -142,8 +136,8 @@ bool Cartridge::readRom(const std::string &fname, bool verbose/*=false*/){
 	while(true){
 		// Read an entire bank at a time.
 		rom.read((char*)getPtrToBank(currRomBank++), 16384);
-		
-		if(rom.eof() || !rom.good()) break;
+		if(rom.eof() || !rom.good() || (currRomBank >= nBanks)) 
+			break;
 	}
 	
 	// Make the ROM read-only
@@ -180,7 +174,7 @@ unsigned int Cartridge::readHeader(std::ifstream &f){
 	
 	// Initialize ROM storage
 	mem.clear();
-	switch(romSize){
+	switch (romSize) {
 		case 0x00: // 32 kB (2 bank)
 			initialize(16384, 2);
 			break;
@@ -207,7 +201,7 @@ unsigned int Cartridge::readHeader(std::ifstream &f){
 			break;
 		case 0x52: // 1.1 MB (72 banks)
 			initialize(16384, 72);
-			break;		
+			break;
 		case 0x53: // 1.2 MB (80 banks)
 			initialize(16384, 80);
 			break;
@@ -237,7 +231,7 @@ unsigned int Cartridge::readHeader(std::ifstream &f){
 	
 	// Set the default ROM bank for SWAP
 	bs = 1;
-	
+
 	return 79;
 }
 

.gitignore
@@ -2,3 +2,8 @@ install/
 build/
 roms/
 boot/
+
+# Visual Studio
+out/
+.vs/
+CMakeSettings.json

CMakeLists.txt
@@ -2,25 +2,30 @@ cmake_minimum_required (VERSION 2.8)
 
 project (gbc)
 
-#Compile with c++11 or c++0x depending on the compiler version
-if (CMAKE_COMPILER_IS_GNUCXX)
-	if (${CMAKE_CXX_COMPILER_VERSION} VERSION_GREATER "4.3" AND
-            ${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS "4.7")
-            set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x")
-    elseif(${CMAKE_CXX_COMPILER_VERSION} VERSION_GREATER "4.6")
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
-    else()
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
-    endif(${CMAKE_CXX_COMPILER_VERSION} VERSION_GREATER "4.3" AND
-            ${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS "4.7")
+if(NOT WIN32) # Unix systems (Ubuntu)
+	#Compile with c++11 or c++0x depending on the compiler version
+	if (CMAKE_COMPILER_IS_GNUCXX)
+		if (${CMAKE_CXX_COMPILER_VERSION} VERSION_GREATER "4.3" AND
+				${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS "4.7")
+				set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x")
+		elseif(${CMAKE_CXX_COMPILER_VERSION} VERSION_GREATER "4.6")
+			set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
+		else()
+			set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
+		endif(${CMAKE_CXX_COMPILER_VERSION} VERSION_GREATER "4.3" AND
+				${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS "4.7")
 
-    #Make compiler messages nice with colored tags.
-	if (${CMAKE_CXX_COMPILER_VERSION} VERSION_GREATER "4.9")
-		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fdiagnostics-color=auto")
+		#Make compiler messages nice with colored tags.
+		if (${CMAKE_CXX_COMPILER_VERSION} VERSION_GREATER "4.9")
+			set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fdiagnostics-color=auto")
+		endif()
+
+		#set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -pedantic -fPIC")
+		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ")
 	endif()
-
-	#set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -pedantic -fPIC")
-	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ")
+else()
+	add_definitions(-DWIN32)
+	message(STATUS "Compiling for Windows")
 endif()
 
 #if user does not specify prefix we assign it to the exec directory
@@ -43,7 +48,9 @@ message(STATUS "Build type: ${CMAKE_BUILD_TYPE}")
 
 #Add additional CXX flags if we use the Debug option
 if(CMAKE_BUILD_TYPE MATCHES "Debug")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pg")
+	if(NOT WIN32)
+		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pg")
+	endif()
 endif(CMAKE_BUILD_TYPE MATCHES "Debug")
 
 #------------------------------------------------------------------------------
@@ -66,8 +73,41 @@ endif()
 
 add_definitions(-DTOP_DIRECTORY="${TOP_DIRECTORY}")
 
+#Set the graphics library
+if(NOT GRAPHICS_LIBRARY)
+	set(GRAPHICS_LIBRARY OpenGL CACHE STRING "Graphics library, options are: SDL OpenGL" FORCE)
+endif()
+
 if(GRAPHICS_LIBRARY MATCHES "OpenGL")
+	# To install OpenGL and freeglut:
+	#(Ubuntu) sudo apt-get install libglu1-mesa-dev freeglut3-dev mesa-common-dev
+	#(Windows) https://www.transmissionzero.co.uk/software/freeglut-devel/
+	if(NOT WIN32)
+		set(GRAPHICS_LIBS -lglut -lGLU -lGL)
+	else()
+		#Find OpenGL install on Windows
+		if(NOT GRAPHICS_LIBRARY)
+			set(FREEGLUT_DIRECTORY "" CACHE STRING "Freeglut install directory" FORCE)
+		endif(NOT GRAPHICS_LIBRARY)
+		set(FREEGLUT_INCLUDE ${FREEGLUT_DIRECTORY}/include)
+		if(CMAKE_CL_64)
+			set(FREEGLUT_LIBRARY ${FREEGLUT_DIRECTORY}/lib/x64/freeglut.lib)
+			set(FREEGLUT_DLL ${FREEGLUT_DIRECTORY}/bin/x64/freeglut.dll)
+		else()
+			set(FREEGLUT_LIBRARY ${FREEGLUT_DIRECTORY}/lib/freeglut.lib)
+			set(FREEGLUT_DLL ${FREEGLUT_DIRECTORY}/bin/freeglut.dll)
+		endif(CMAKE_CL_64)
+		include_directories(${FREEGLUT_INCLUDE})
+		set(GRAPHICS_LIBS ${FREEGLUT_LIBRARY} opengl32.lib)
+		#It is necessary to copy freeglut.dll to the build directory containing the executable.
+		#Alternatively it may be copied to the Windows system directory.
+		#This is necessary so that the freeglut headers point to the correct library
+		add_definitions(-DNDEBUG)
+	endif()
 	add_definitions(-DUSE_OPENGL)
+elseif(GRAPHICS_LIBRARY MATCHES "SDL")
+	#Fix this at some point for Windows!
+	set(GRAPHICS_LIBS -lSDL2)
 endif()
 
 #Add the include directories.
@@ -80,13 +120,6 @@ if(BUILD_DEBUG)
 	find_package (QT REQUIRED)
 	include_directories(qtfiles ${QT_INC_DIR} ${QT_CORE_INC_DIR} ${QT_GUI_INC_DIR})
 	add_subdirectory(qtfiles)
-endif()
-
-#Set the graphics library
-if(NOT GRAPHICS_LIBRARY)
-	# To install OpenGL:
-	#sudo apt-get install libglu1-mesa-dev freeglut3-dev mesa-common-dev
-	set(GRAPHICS_LIBRARY OpenGL CACHE STRING "Graphics library, options are: SDL OpenGL" FORCE)
 endif()
 
 #Set the Gameboy boot ROM

assets/default.cfg
@@ -1,0 +1,10 @@
+ROM_DIRECTORY           C:/Path/To/Roms
+ROM_FILENAME            rom.gb
+FRAMERATE_MULTIPLIER    1.0
+VERBOSE_MODE            false
+PIXEL_SCALE             2
+FORCE_COLOR_MODE        false
+AUTO_SAVE_SRAM          true
+DEBUG_MODE              false
+OPEN_TILE_VIEWER        false
+OPEN_LAYER_VIEWER       false

graphics/include/colors.hpp
@@ -122,15 +122,15 @@ private:
 
 namespace Colors{
 	const ColorRGB BLACK(0, 0, 0);
-	const ColorRGB DKGRAY(2/3.0, 2/3.0, 2/3.0);
-	const ColorRGB LTGRAY(1/3.0, 1/3.0, 1/3.0);
+	const ColorRGB DKGRAY(2/3.0f, 2/3.0f, 2/3.0f);
+	const ColorRGB LTGRAY(1/3.0f, 1/3.0f, 1/3.0f);
 	const ColorRGB WHITE(1, 1, 1);
 	
 	// Monochrome colors (GB)
-	const ColorRGB GB_DKSTGREEN(15.0/255,  56.0/255,  15.0/255);
-	const ColorRGB GB_DKGREEN(  48.0/255,  98.0/255,  48.0/255);
-	const ColorRGB GB_LTGREEN(  139.0/255, 172.0/255, 15.0/255);
-	const ColorRGB GB_GREEN(    155.0/255, 188.0/255, 15.0/255);
+	const ColorRGB GB_DKSTGREEN(15.0f/255,  56.0f/255,  15.0f/255);
+	const ColorRGB GB_DKGREEN(  48.0f/255,  98.0f/255,  48.0f/255);
+	const ColorRGB GB_LTGREEN(  139.0f/255, 172.0f/255, 15.0f/255);
+	const ColorRGB GB_GREEN(    155.0f/255, 188.0f/255, 15.0f/255);
 	
 	// Primary colors
 	const ColorRGB RED(1, 0, 0);

graphics/source/CMakeLists.txt
@@ -1,18 +1,14 @@
-if(GRAPHICS_SOURCES MATCHES "SDL")
-	# To install SDL:
-	#sudo apt-get install libsdl2-dev
-	set(GRAPHICS_SOURCES colors.cpp GraphicsSDL.cpp)
-	set(GRAPHICS_LIBS -lSDL2 PARENT_SCOPE)
-elseif(GRAPHICS_LIBRARY MATCHES "OpenGL")
+if(GRAPHICS_LIBRARY MATCHES "OpenGL")
 	# To install OpenGL:
 	#sudo apt-get install libglu1-mesa-dev freeglut3-dev mesa-common-dev
 	set(GRAPHICS_SOURCES colors.cpp GraphicsOpenGL.cpp)
-	set(GRAPHICS_LIBS -lglut -lGLU -lGL PARENT_SCOPE)
+elseif(GRAPHICS_LIBRARY MATCHES "SDL")
+	# To install SDL:
+	#sudo apt-get install libsdl2-dev
+	set(GRAPHICS_SOURCES colors.cpp GraphicsSDL.cpp)
 else()
 	message(STATUS "Unsupported renderer (${GRAPHICS_LIBRARY})")
 endif()
-
-set(TEST_VAR "pizza" PARENT_SCOPE)
 
 # Generate the graphics library
 add_library(GRAPHICS_OBJECTS OBJECT ${GRAPHICS_SOURCES})

graphics/source/GraphicsOpenGL.cpp
@@ -55,12 +55,13 @@ void handleSpecialKeys(int key, int x, int y){
 		case GLUT_KEY_RIGHT:
 			ckey = 0x4F;
 			break;
-		case GLUT_KEY_F1 ... GLUT_KEY_F12: // Function keys
-			ckey = 0xF0;
-			ckey |= (key & 0x0F);
-			break;
 		default:
-			return;
+			if (key >= GLUT_KEY_F1 && key <= GLUT_KEY_F12) { // Function keys
+				ckey = 0xF0;
+				ckey |= (key & 0x0F);
+			}
+			else { return; }
+			break;
 	}
 	if(ckey)
 		handleKeys(ckey, x, y);
@@ -109,11 +110,11 @@ void reshapeScene(GLint width, GLint height){
 	float aspect = float(wprime)/hprime;
 
 	if(aspect > currentWindow->getAspectRatio()){ // Wider window (height constrained)
-		wprime = currentWindow->getAspectRatio() * hprime;
+		wprime = (int)(currentWindow->getAspectRatio() * hprime);
 		glPointSize(float(hprime)/currentWindow->getHeight());
 	}
 	else{ // Taller window (width constrained)
-		hprime = wprime / currentWindow->getAspectRatio();
+		hprime = (int)(wprime / currentWindow->getAspectRatio());
 		glPointSize(float(wprime)/currentWindow->getWidth());
 	}
 
@@ -129,6 +130,10 @@ void reshapeScene(GLint width, GLint height){
 	
 	// Clear the window.
 	currentWindow->clear();
+}
+
+void displayFunction() {
+	// This callback does nothing, but is required on Windows
 }
 
 /////////////////////////////////////////////////////////////////////
@@ -268,6 +273,9 @@ void Window::initialize(){
 
 	// Set window size handler
 	glutReshapeFunc(reshapeScene);
+
+	// Set the display function callback (required for Windows)
+	glutDisplayFunc(displayFunction);
 
 	init = true;
 }

graphics/source/colors.cpp
@@ -47,34 +47,44 @@
 
 	void ColorRGB::toGrayscale(){
 		// Based on the sRGB convention
-		r *= 0.2126;
-		g *= 0.7152;
-		b *= 0.0722;
+		r *= 0.2126f;
+		g *= 0.7152f;
+		b *= 0.0722f;
 	}
 
 #endif
 
 ColorRGB ColorRGB::operator + (const ColorRGB &rhs) const {
-	float rprime = (r + rhs.r)/255.0;
-	float gprime = (g + rhs.g)/255.0;
-	float bprime = (b + rhs.b)/255.0;
+	float rprime = (r + rhs.r)/255.0f;
+	float gprime = (g + rhs.g)/255.0f;
+	float bprime = (b + rhs.b)/255.0f;
 	return ColorRGB((rprime <= 1 ? rprime : 1), (gprime <= 1 ? gprime : 1), (bprime <= 1 ? bprime : 1));
 }
 
 ColorRGB ColorRGB::operator - (const ColorRGB &rhs) const {
-	float rprime = (r - rhs.r)/255.0;
-	float gprime = (g - rhs.g)/255.0;
-	float bprime = (b - rhs.b)/255.0;
+	float rprime = (r - rhs.r)/255.0f;
+	float gprime = (g - rhs.g)/255.0f;
+	float bprime = (b - rhs.b)/255.0f;
 	return ColorRGB((rprime > 0 ? rprime : 0), (gprime > 1 ? gprime : 0), (bprime > 1 ? bprime : 0));
 }
 
+#ifdef USE_OPENGL
 ColorRGB ColorRGB::operator * (const float &rhs) const {
-	return ColorRGB(toFloat(r)*rhs, toFloat(g)*rhs, toFloat(b)*rhs);
+	return ColorRGB(r*rhs, g*rhs, b*rhs);
 }
 
 ColorRGB ColorRGB::operator / (const float &rhs) const {
-	return ColorRGB(toFloat(r)/rhs, toFloat(g)/rhs, toFloat(b)/rhs);
+	return ColorRGB(r/rhs, g/rhs, b/rhs);
 }
+#else
+ColorRGB ColorRGB::operator * (const float& rhs) const {
+	return ColorRGB(toFloat(r) * rhs, toFloat(g) * rhs, toFloat(b) * rhs);
+}
+
+ColorRGB ColorRGB::operator / (const float& rhs) const {
+	return ColorRGB(toFloat(r) / rhs, toFloat(g) / rhs, toFloat(b) / rhs);
+}
+#endif
 
 ColorRGB& ColorRGB::operator += (const ColorRGB &rhs){
 	return ((*this) = (*this) + rhs);

commit dcc70394a8ece48902a29f90ab9a7da9de6de162

Fixed ROM MBC logic error introduced in 7e56062
Fixed cartridge ROM MBC logic error causing certain games using
MBC1 memory controller to hang or crash.

source/Cartridge.cpp
@@ -61,7 +61,7 @@ bool Cartridge::writeRegister(const unsigned short &reg, const unsigned char &va
 		case CartMBC::ROMONLY: // ROM only
 			break;
 		case CartMBC::MBC1: // MBC1 (1-3)
-			if(reg <= 0x2000){ // RAM enable (write only)
+			if(reg < 0x2000){ // RAM enable (write only)
 				// Any value written to this area with 0x0A in its lower 4 bits will enable cartridge RAM
 				// Note: Type 0x01 does not have cartridge RAM
 				extRamEnabled = ((val & 0x0F) == 0x0A);