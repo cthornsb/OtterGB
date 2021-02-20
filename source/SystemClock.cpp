cthornsb/OtterGB

commit aa2495b7e1a604b4842bebff5e1bd6b2764b4235

Fixed not checking LYC=LY coincidence when LY=0
Fixed SystemClock not checking LYC coincidence when LY is reset.
This fixes severe graphical issues for games which use LY=LYC=0
STAT interrupt.

Changed SystemClock LCD driver cycle counter to pixel clock
(~4 MHz). Added ability to briefly pause pixel clock (effectively
delaying start of horizontal blank interval) due to the number of
sprites which were drawn on a scanline.

Updated SystemClock class documentation.

source/SystemClock.cpp
@@ -5,21 +5,22 @@
 #include "SystemRegisters.hpp"
 #include "SystemGBC.hpp"
 
+// System clock
 constexpr unsigned int SYSTEM_CLOCK_FREQUENCY = 1048576; // Hz
 
+// Pixel clock
 constexpr unsigned int MODE2_START = 0;     // STAT mode 2 from 0 to 79 (80 cycles)
-constexpr unsigned int MODE3_START = 20;    // STAT mode 3 from 80 to 251 (172 cycles)
-constexpr unsigned int MODE0_START = 63;    // STAT mode 0 from 252 to 456 (204 cycles)
-constexpr unsigned int MODE1_START = 16416; // STAT mode 1 from 65664 to 70224 (4560 cycles)
-
-constexpr unsigned int VERTICAL_SYNC_CYCLES   = 17556; // CPU cycles per VSYNC (~59.73 Hz)
-constexpr unsigned int HORIZONTAL_SYNC_CYCLES = 114;   // CPU cycles per HSYNC (per 154 scanlines)
+constexpr unsigned int MODE3_START = 80;    // STAT mode 3 from 80 to 251 (172 cycles)
+constexpr unsigned int MODE0_START = 252;   // STAT mode 0 from 252 to 456 (204 cycles)
+constexpr unsigned int MODE1_START = 65664; // STAT mode 1 from 65664 to 70224 (4560 cycles)
+constexpr unsigned int VERTICAL_SYNC_CYCLES   = 70224; // CPU cycles per VSYNC (~59.73 Hz)
+constexpr unsigned int HORIZONTAL_SYNC_CYCLES = 456;   // CPU cycles per HSYNC (per 154 scanlines)
 
 SystemClock::SystemClock() : 
 	SystemComponent("Clock", 0x204b4c43), // "CLK "
 	vsync(false), 
-	cyclesSinceLastVSync(0), 
-	cyclesSinceLastHSync(0),
+	cyclesSinceLastVSync(VERTICAL_SYNC_CYCLES), 
+	cyclesSinceLastHSync(HORIZONTAL_SYNC_CYCLES),
 	currentClockSpeed(0),
 	cyclesPerVSync(0),
 	cyclesPerHSync(0),
@@ -30,7 +31,9 @@ SystemClock::SystemClock() :
 	timeOfLastVSync(hrclock::now()),
 	cycleTimer(hrclock::now()),
 	cycleCounter(0),
-	cyclesPerSecond(0)
+	cyclesPerSecond(0),
+	nClockPause(0),
+	modeStart()
 {
 	setNormalSpeedMode();
 }
@@ -43,11 +46,13 @@ void SystemClock::setDoubleSpeedMode(){
 	waitUntilNextVSync();
 	currentClockSpeed = SYSTEM_CLOCK_FREQUENCY * 2;
 	modeStart[0] = MODE0_START * 2;
-	modeStart[1] = MODE1_START * 2;
-	modeStart[2] = MODE2_START * 2;
-	modeStart[3] = MODE3_START * 2;
+	modeStart[1] = VERTICAL_SYNC_CYCLES * 2;
+	modeStart[2] = HORIZONTAL_SYNC_CYCLES * 2;
+	modeStart[3] = HORIZONTAL_SYNC_CYCLES * 2;
 	cyclesPerVSync = VERTICAL_SYNC_CYCLES * 2;
 	cyclesPerHSync = HORIZONTAL_SYNC_CYCLES * 2;
+	cyclesSinceLastVSync = 0;
+	cyclesSinceLastHSync = 0;
 	framePeriod = 1E6*VERTICAL_SYNC_CYCLES/SYSTEM_CLOCK_FREQUENCY; // in microseconds
 }
 
@@ -60,7 +65,9 @@ void SystemClock::setNormalSpeedMode(){
 	modeStart[3] = MODE3_START;
 	cyclesPerVSync = VERTICAL_SYNC_CYCLES;
 	cyclesPerHSync = HORIZONTAL_SYNC_CYCLES;
-	framePeriod = 1E6*VERTICAL_SYNC_CYCLES/SYSTEM_CLOCK_FREQUENCY; // in microseconds
+	cyclesSinceLastVSync = 0;
+	cyclesSinceLastHSync = 0;
+	framePeriod = 1E6*VERTICAL_SYNC_CYCLES/(4 * SYSTEM_CLOCK_FREQUENCY); // in microseconds
 }
 
 // Tick the system clock.
@@ -80,16 +87,16 @@ bool SystemClock::onClockUpdate(){
 	}
 
 	// Check if the display is enabled. If it's not, set STAT to mode 1
-	if(!rLCDC->getBit(7)){
+	if(!rLCDC->bit7()){
 		if(lcdDriverMode != 1){
-			lcdDriverMode = 1;
-			rSTAT->setBits(0,1,1); // Mode 1
+			startMode1();
 		}
 		return false;
 	}
 
-	cyclesSinceLastVSync++;
-	cyclesSinceLastHSync++;
+	// Tick the pixel clock (~4 MHz)
+	cyclesSinceLastVSync += 4;
+	cyclesSinceLastHSync += 4;
 
 	// Check if we're on the next scanline (every 456 cycles)
 	// Set the LCD STAT register mode flag:
@@ -98,46 +105,40 @@ bool SystemClock::onClockUpdate(){
 	//  10 scanlines of overscan takes 4560 cycles (VBlank) (lines 144-153)
 	//  The mode progression is 2->3->0->2->3->0 ... ->2->3->0->1->2 ...	
 	if(cyclesSinceLastVSync <= modeStart[1]){ // Visible scanlines (0-143)
-		// 0 to 20   : mode2 - OAM sprite search (20 cycles)
-		// 20 to 63  : mode3 - Drawing pixels (43+X cycles)
-		// 63 to 114 : mode0 - HBlank (51-X cycles)
-		// ...
-		// 16416 to 17556 : mode1 - VBlank
+		// Pixel clock (~4 MHz)
+		// 0 to 80        : mode 2 (80 ticks)
+		// 80 to 248+X    : mode 3 (168 to 291 ticks, X in range 0 to 123)
+		// 248+X to 456   : mode 0 (85 to 208 ticks, depending on X) 
+		// ...            : 143 more scanlines (143 * 456 = 65208 ticks)
+		// 65664 to 70224 : mode 1 (4560 ticks)
 		if(cyclesSinceLastHSync == modeStart[3]){ // Mode 2->3 (Drawing the scanline)
-			lcdDriverMode = 3;
-			rSTAT->setBits(0,1); // Mode 3
-			sys->handleHBlankPeriod(); // Start drawing the scanline
-		}		
-		else if(cyclesSinceLastHSync == modeStart[0]){ // Mode 3->0 (HBlank)
-			lcdDriverMode = 0;
-			rSTAT->resetBits(0,1); // Mode 0
-			mode0Interrupt();
-		}		
+			startMode3();
+		}
+		else if(
+			cyclesSinceLastHSync % (modeStart[0] - nClockPause) < 4 && 
+			cyclesSinceLastHSync / (modeStart[0] - nClockPause) == 1)
+		{ // Mode 3->0 (HBlank), delayed up to 123 pixel clock ticks
+			startMode0();
+		}	
 		else if(cyclesSinceLastHSync == cyclesPerHSync){ // Mode 0->2(1)
 			incrementScanline(); // Increment the scanline
 			if(cyclesSinceLastVSync != modeStart[1]){ // Mode 0->2 (Reading from OAM)
-				lcdDriverMode = 2;
-				rSTAT->setBits(0,1,2); // Mode 2
-				mode2Interrupt();
+				startMode2();
 			}
 			else{ // Mode 0->1 (VBlank, overscan)
-				lcdDriverMode = 1;
-				rSTAT->setBits(0,1,1); // Mode 1
-				mode1Interrupt();
-				vsync = true;
+				startMode1(); // vsync flag set to true
 			}
 		}
 	}
-	else if(cyclesSinceLastVSync <= cyclesPerVSync){ // Mode 1 - Overscan (144-153)
+	else if(cyclesSinceLastVSync < cyclesPerVSync){ // Mode 1 - Overscan (144-153)
 		if(cyclesSinceLastHSync == cyclesPerHSync){
 			incrementScanline();
 		}
 	}
 	else{ // Start the next frame (Mode 1->2)
-		rSTAT->setBits(0,1,2); // Mode 2
-		waitUntilNextVSync();	
+		waitUntilNextVSync(); // vsync flag set to false
 		resetScanline(); // VBlank period has ended, next frame started
-		mode2Interrupt();
+		//startMode2(); // resetScanline() automatically starts mode 2
 	}
 	
 	return vsync;
@@ -151,29 +152,29 @@ void SystemClock::resetScanline(){
 	vsync = false;
 	cyclesSinceLastVSync = 0;
 	cyclesSinceLastHSync = 0;	
-	if(rLCDC->getBit(7)){ // LCD enabled
-		lcdDriverMode = 2;
-		rSTAT->setBits(0,1,2); // Mode 2
+	if(rLCDC->bit7()){ // LCD enabled
+		startMode2();
 	}
 	else{ // LCD disabled
-		lcdDriverMode = 1;
-		rSTAT->setBits(0,1,1); // Mode 1
+		startMode1();
 	}
 	rLY->clear();
 	rWLY->clear();
+	compareScanline(); // Handle LY / LYC coincidence
 }
 
-/** Increment the current scanline (register LY).
-  * @return True if there is coincidence with register LYC, and return false otherwise.
-  */
-bool SystemClock::incrementScanline(){
+void SystemClock::incrementScanline(){
 	cyclesSinceLastHSync = 0; // Reset HSync cycle count
 	(*rLY)++; // Increment LY register
+	compareScanline(); // Handle LY / LYC coincidence
+}
+
+bool SystemClock::compareScanline(){
 	if((*rLY) != (*rLYC)) // LY != LYC
-		rSTAT->resetBit(2); // Reset bit 2 of STAT (coincidence flag)
+		rSTAT->resetBit2(); // Reset bit 2 of STAT (coincidence flag)
 	else{ // LY == LYC
-		rSTAT->setBit(2); // Set bit 2 of STAT (coincidence flag)
-		if(rSTAT->getBit(6)) // Issue LCD STAT interrupt
+		rSTAT->setBit2(); // Set bit 2 of STAT (coincidence flag)
+		if(rSTAT->bit6()) // Issue LCD STAT interrupt
 			sys->handleLcdInterrupt();
 		return true;
 	}
@@ -194,25 +195,40 @@ void SystemClock::waitUntilNextVSync(){
 		frameCount = 0;
 	}
 	timeOfLastVSync = hrclock::now();
-	vsync = false;
 }
 
-void SystemClock::mode0Interrupt(){
-	if(rSTAT->getBit(3)){ // Request LCD STAT interrupt (INT 48)
+void SystemClock::startMode0(){
+	lcdDriverMode = 0;
+	rSTAT->setBits(0, 1, 0x0);
+	sys->lockMemory(false, false); // VRAM and OAM now accessible
+	if(rSTAT->bit3()){ // Request LCD STAT interrupt (INT 48)
 		sys->handleLcdInterrupt();
 	}
 }
 
-void SystemClock::mode1Interrupt(){
+void SystemClock::startMode1(){
+	vsync = true;
+	lcdDriverMode = 1;
+	rSTAT->setBits(0, 1, 0x1);
+	sys->lockMemory(false, false); // VRAM and OAM now accessible
+	if(rSTAT->bit4())
+		sys->handleLcdInterrupt(); // Request LCD STAT interrupt (INT 48)
 	sys->handleVBlankInterrupt(); // Request VBlank interrupt (INT 40)
-	if(rSTAT->getBit(4)) // Request LCD STAT interrupt (INT 48)
+}
+
+void SystemClock::startMode2(){
+	lcdDriverMode = 2;
+	rSTAT->setBits(0, 1, 0x2);
+	sys->lockMemory(true, false); // OAM inaccessible
+	if(rSTAT->bit5()) // Request LCD STAT interrupt (INT 48)
 		sys->handleLcdInterrupt();
 }
 
-void SystemClock::mode2Interrupt(){
-	if(rSTAT->getBit(5)){ // Request LCD STAT interrupt (INT 48)
-		sys->handleLcdInterrupt();
-	}
+void SystemClock::startMode3(){
+	lcdDriverMode = 3;
+	rSTAT->setBits(0, 1, 0x3);
+	sys->lockMemory(true, true); // VRAM and OAM inaccessible
+	sys->handleHBlankPeriod(); // Start drawing the scanline
 }
 
 void SystemClock::userAddSavestateValues(){

source/SystemGBC.cpp
@@ -459,8 +459,9 @@ bool SystemGBC::execute(){
 
 void SystemGBC::handleHBlankPeriod(){
 	if(!emulationPaused){
-		if(nFrames % frameSkip == 0)
-			gpu->drawNextScanline(oam.get());
+		if(nFrames % frameSkip == 0){
+			sclk->setPixelClockPause( gpu->drawNextScanline(oam.get()) );
+		}
 		dma->onHBlank();
 	}
 #ifdef USE_QT_DEBUGGER

include/SystemClock.hpp
@@ -6,88 +6,170 @@
 
 class SystemClock : public SystemComponent {
 public:
+	/** Default constructor
+	  */
 	SystemClock();
 
-	double getFramerate() const { return framerate; }
+	/** Get the instantaneous framerate (in frames per second) computed once per second
+	  */
+	double getFramerate() const { 
+		return framerate; 
+	}
 	
+	/** Set the target framerate multiplier
+	  * @param freq Framerate multiplier where a multiplier of 1.0 corresponds to normal output framerate of 59.7 fps
+	  */
 	void setFramerateMultiplier(const float &freq);
 
+	/** Set system clock to CGB double speed mode (~2 MHz), effectively doubling the number of instructions per second
+	  * The CPU, system timer, serial controller, and DMA controller will operate at double speed while
+	  * the PPU, HDMA controller, and APU will all operate at normal speed.
+	  * Note: Changing clock speed does directly affect output framerate
+	  */
 	void setDoubleSpeedMode();
 	
+	/** Set system clock to normal speed mode (~1 MHz)
+	  * Note: Changing clock speed does not directly affect output framerate
+	  */
 	void setNormalSpeedMode();
 
-	double getFrequency() const { return currentClockSpeed; }
-
-	double getCyclesPerSecond() const { return cyclesPerSecond; }
-	
-	unsigned int getCyclesSinceVBlank() const { return cyclesSinceLastVSync; }
-	
-	unsigned int getCyclesSinceHBlank() const { return cyclesSinceLastHSync; }
-	
-	unsigned char getDriverMode() const { return lcdDriverMode; }
-	
-	bool getVSync() const { return vsync; }
-	
-	/** Poll the VSync (VBlank) flag and reset it.
+	/** Get the current system clock speed in cycles per second
 	  */
-	bool pollVSync(){ return (vsync ? !(vsync = false) : false); }
+	double getFrequency() const { 
+		return currentClockSpeed; 
+	}
+
+	/** Get the average number of clock cycles per second over a ten second period
+	  */
+	double getCyclesPerSecond() const { 
+		return cyclesPerSecond; 
+	}
 	
-	/** Perform one clock tick.
-	  * @return True if the system has entered VBlank, and false otherwise.
+	/** Get the number of clock cycles since the previous vertical blanking (VBlank) interval ended
+	  */
+	unsigned int getCyclesSinceVBlank() const { 
+		return cyclesSinceLastVSync; 
+	}
+	
+	/** Get the number of clock cycles since the previous horizontal blanking (HBlank) interval ended
+	  */
+	unsigned int getCyclesSinceHBlank() const { 
+		return cyclesSinceLastHSync; }
+	
+	/** Get the current LCD driver mode
+	  *  0: Horizontal blank (HBlank) interval
+	  *  1: Vertical blank (VBlank) interval
+	  *  2: PPU is searching OAM for sprite coordinates (OAM inaccessible)
+	  *  3: PPU is drawing the current scanline (VRAM and OAM inaccessible)
+	  */
+	unsigned char getDriverMode() const { 
+		return lcdDriverMode; 
+	}
+	
+	/** Return true if the LCD driver is in vertical blank (VBlank) interval
+	  */
+	bool getVSync() const { 
+		return vsync; 
+	}
+	
+	/** Set the number of pixel clock (~4 MHz) ticks to delay start of HBlank interval (mode 0)
+	  * This delay is due to several factors, the primary one being the number of sprites the PPU drew on the current scanline
+	  */
+	void setPixelClockPause(const unsigned int& ticks){
+		nClockPause = ticks;
+	}
+	
+	/** Poll the VSync (vertical blank interval) flag and reset it.
+	  */
+	bool pollVSync(){ 
+		return (vsync ? !(vsync = false) : false); 
+	}
+	
+	/** Perform one system clock tick
+	  * @return True if the system has entered vertical blank (VBlank) interval, and false otherwise
 	  */
 	bool onClockUpdate() override ;
 	
-	/** Sleep until the start of the next VSync cycle (i.e. wait until the
-	  * start of the next frame). Useful for maintaining desired framerate
-	  * without advancing the system clock.
+	/** Sleep until the start of the next VSync cycle (i.e. wait until the start of the next frame)
+	  * Useful for maintaining desired framerate without advancing the system clock.
 	  */
 	void wait();
 	
-	/** Reset cycle counters.
+	/** Reset cycle counters, zero LY register, and handle LY - LYC coincidence check
+	  * If LCDC register bit 7 is set (LCD enabled) LCD driver mode is set to 2, otherwise driver mode is set to 1
 	  */
 	void resetScanline();
 
 private:
-	bool vsync;
+	bool vsync; ///< Set if LCD driver is in vertical blank interval
 
-	unsigned int cyclesSinceLastVSync; ///< The number of cycles since the last Vertical Sync
-	unsigned int cyclesSinceLastHSync; ///< The number of cycles since the last  Horizontal Sync
+	unsigned int cyclesSinceLastVSync; ///< Number of pixel clock ticks since the last vertical blank interval
+	
+	unsigned int cyclesSinceLastHSync; ///< Number of pixel clock ticks since the last horizontal blank interval
 
-	unsigned int currentClockSpeed; ///< The current number of machine clock cycles per second
-	unsigned int cyclesPerVSync; ///< The current number of clock cycles per vertical sync
-	unsigned int cyclesPerHSync; ///< The current number of clock cycles per horizontal sync
+	unsigned int currentClockSpeed; ///< Current number of system clock cycles per second
+	
+	unsigned int cyclesPerVSync; ///< Current number of pixel clock ticks per vertical sync
+	
+	unsigned int cyclesPerHSync; ///< Current number of pixel clock ticks per horizontal sync
 
-	unsigned char lcdDriverMode;
+	unsigned char lcdDriverMode; ///< Current LCD driver mode (0-3)
 
-	double framerate;
+	double framerate; ///< Instantaneous framerate computed once per second
 	
 	double framePeriod; ///< Wall clock time between successive frames (microseconds)
 
-	hrclock::time_point timeOfInitialization; ///< The time that the system clock was initialized
-	hrclock::time_point timeOfLastVSync; ///< The time at which the screen was last refreshed
-	hrclock::time_point cycleTimer;
+	hrclock::time_point timeOfInitialization; ///< Time that the system clock was initialized
+	
+	hrclock::time_point timeOfLastVSync; ///< Time at which the screen was last refreshed
+	
+	hrclock::time_point cycleTimer; ///< The time taken to execute ten times the current clock speed of cycles
 
-	unsigned int cycleCounter;
+	unsigned int cycleCounter; ///< System clock cycle counter
 
-	double cyclesPerSecond;
+	double cyclesPerSecond; ///< Average number of system clock cycles per second over a ten second period
 
-	unsigned int modeStart[4];
+	unsigned int nClockPause; ///< Number of pixel clock ticks to pause before entering HBlank period (mode 0)
 
-	/** Increment the current scanline (register LY).
-	  * @return True if there is coincidence with register LYC, and return false otherwise.
+	unsigned int modeStart[4]; ///< Number of pixel clock ticks to elapse before starting the next LCD driver mode
+
+	/** Increment the current scanline (register LY)
 	  */
-	bool incrementScanline();
+	void incrementScanline();
+
+	/** Handle coincidence between LY and LYC registers
+	  * Updates STAT register coincidence bit and issues LCD STAT interrupt request if enabled.
+	  * @return True if LY == LYC, and return false otherwise
+	  */
+	bool compareScanline();
 
 	/** Sleep for the required amount of time to maintain the set framerate.
 	  */
 	void waitUntilNextVSync();
 
-	void mode0Interrupt();
+	/** Begin mode 0 (horizontal blank period)
+	  * Request mode 0 STAT interrupt (INT 48, if enabled)
+	  */
+	void startMode0();
 
-	void mode1Interrupt();
+	/** Begin mode 1 (vertical blank period)
+	  * Request mode 1 STAT interrupt (INT 48, if enabled)
+	  * Request VBlank interrupt (INT 40)
+	  */
+	void startMode1();
 
-	void mode2Interrupt();
-
+	/** Begin mode 2 (OAM search)
+	  * OAM inaccessible
+	  * Request mode 2 STAT interrupt (INT 48, if enabled)
+	  */
+	void startMode2();
+	
+	/** Begin mode 3 (drawing scanline)
+	  * VRAM and OAM inaccessible
+	  * Trigger PPU to start drawing scanline
+	  */
+	void startMode3();
+	
 	/** Add elements to a list of values which will be written to / read from an emulator savestate
 	  */
 	void userAddSavestateValues() override;

commit 9d2b5a43b0a9f7e9c44258e112f224b7629a0d96

Fixed double speed mode system clock error
Fixed timing error introduced in commit aa2495b which caused CGB
double speed mode to hang.

Temporarily disabled VRAM / OAM lock to fix CGB sprites.

source/SystemClock.cpp
@@ -19,8 +19,8 @@ constexpr unsigned int HORIZONTAL_SYNC_CYCLES = 456;   // CPU cycles per HSYNC (
 SystemClock::SystemClock() : 
 	SystemComponent("Clock", 0x204b4c43), // "CLK "
 	vsync(false), 
-	cyclesSinceLastVSync(VERTICAL_SYNC_CYCLES), 
-	cyclesSinceLastHSync(HORIZONTAL_SYNC_CYCLES),
+	cyclesSinceLastVSync(0), 
+	cyclesSinceLastHSync(0),
 	currentClockSpeed(0),
 	cyclesPerVSync(0),
 	cyclesPerHSync(0),
@@ -46,14 +46,14 @@ void SystemClock::setDoubleSpeedMode(){
 	waitUntilNextVSync();
 	currentClockSpeed = SYSTEM_CLOCK_FREQUENCY * 2;
 	modeStart[0] = MODE0_START * 2;
-	modeStart[1] = VERTICAL_SYNC_CYCLES * 2;
-	modeStart[2] = HORIZONTAL_SYNC_CYCLES * 2;
-	modeStart[3] = HORIZONTAL_SYNC_CYCLES * 2;
+	modeStart[1] = MODE1_START * 2;
+	modeStart[2] = MODE2_START * 2;
+	modeStart[3] = MODE3_START * 2;
 	cyclesPerVSync = VERTICAL_SYNC_CYCLES * 2;
 	cyclesPerHSync = HORIZONTAL_SYNC_CYCLES * 2;
 	cyclesSinceLastVSync = 0;
 	cyclesSinceLastHSync = 0;
-	framePeriod = 1E6*VERTICAL_SYNC_CYCLES/SYSTEM_CLOCK_FREQUENCY; // in microseconds
+	framePeriod = 1E6 * VERTICAL_SYNC_CYCLES / (4 * SYSTEM_CLOCK_FREQUENCY); // in microseconds
 }
 
 void SystemClock::setNormalSpeedMode(){
@@ -67,7 +67,7 @@ void SystemClock::setNormalSpeedMode(){
 	cyclesPerHSync = HORIZONTAL_SYNC_CYCLES;
 	cyclesSinceLastVSync = 0;
 	cyclesSinceLastHSync = 0;
-	framePeriod = 1E6*VERTICAL_SYNC_CYCLES/(4 * SYSTEM_CLOCK_FREQUENCY); // in microseconds
+	framePeriod = 1E6 * VERTICAL_SYNC_CYCLES / (4 * SYSTEM_CLOCK_FREQUENCY); // in microseconds
 }
 
 // Tick the system clock.

source/SystemGBC.cpp
@@ -1248,8 +1248,8 @@ void SystemGBC::resumeUntilNextVBlank(){
 }
 
 void SystemGBC::lockMemory(bool lockVRAM, bool lockOAM){
-	bLockedVRAM = lockVRAM;
-	bLockedOAM = lockOAM;
+	//bLockedVRAM = lockVRAM;
+	//bLockedOAM = lockOAM;
 }
 
 bool SystemGBC::writeRegister(const unsigned short &reg, const unsigned char &val){